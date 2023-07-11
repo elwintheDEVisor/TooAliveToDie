d4463a91c2b22a715738080c3570136178994a4a
Elwin Barredo <126772213+elwintheDEVisor@users.noreply.github.com>
2023-07-11 20:16:23 +0800

    Add files via upload

---
+++ The Butcher/The Butcher/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "                               .=+:               " << endl;
+    sleep(100);
+    cout << "                             -*@@@%               " << endl;
+    sleep(100);
+    cout << "                          :*@@@@%*.               " << endl;
+    sleep(100);
+    cout << "                       .+@@@@%*:                  " << endl;
+    sleep(100);
+    cout << "                   ..=%@@@%*-                     " << endl;
+    sleep(100);
+    cout << "                .=%#*%@@%-                        " << endl;
+    sleep(100);
+    cout << "              -#@##@#**.    :+**=.                " << endl;
+    sleep(100);
+    cout << "           :-=@@@@%%@#+    *@#+++*+               " << endl;
+    sleep(100);
+    cout << "           ###@@@@#==+=:. .%@#*+==#.              " << endl;
+    sleep(100);
+    cout << "            +@@@*   =*#+- =@%##===:               " << endl;
+    sleep(100);
+    cout << "            .@@=.     .%+=.#@#+==-                " << endl;
+    sleep(100);
+    cout << "            :@#        :#*++###+=-.               " << endl;
+    sleep(100);
+    cout << "            *@#:::.::--+@@%*+==*+---:             " << endl;
+    sleep(100);
+    cout << "            *%##%*#%@@@%@%%@#+--%:---.            " << endl;
+    sleep(100);
+    cout << "            .**#%%%@@@@@##@@@@#%%+++-             " << endl;
+    sleep(100);
+    cout << "                ::. .%@@@@@@@@@%#+--:             " << endl;
+    sleep(100);
+    cout << "                    :@@@@@@@#*+==++*:             " << endl;
+    sleep(100);
+    cout << "                    *@@@@@%%*--=-=+=              " << endl;
+    sleep(100);
+    cout << "                   *@@@@@%%#%=--:--.              " << endl;
+    sleep(100);
+    cout << "                  -@@@@@%####*----=               " << endl;
+    sleep(100);
+    cout << "                 .@@@@@@%###***+*+:               " << endl;
+    sleep(100);
+    cout << "                :@@@@@@@@%##*+*++                 " << endl;
+    sleep(100);
+    cout << "               -@@@@@@@@@%%##**##.                " << endl;
+    sleep(100);
+    cout << "              =@@@@@@@@@%%%##****-                " << endl;
+    sleep(100);
+    cout << "             =@@@@@@@@@@%%%####**+                " << endl;
+    sleep(100);
+    cout << "            -@@@@@@@@@@@%%###%%***                " << endl;
+    sleep(100);
+    cout << "            %@@@@@@@@@@%%####@@#**                " << endl;
+    sleep(100);
+    cout << "            .+@@@@@@@@%%####%@@%*#.               " << endl;
+    sleep(100);
+    cout << "              .#@@@@@%%####-:@@@%#+               " << endl;
+    sleep(100);
+    cout << "               #@@@@@%###*=  +@@@%#=              " << endl;
+    sleep(100);
+    cout << "               *@@@@+ .:-=   :@@@@%#              " << endl;
+    sleep(100);
+    cout << "              .%@@@@=         =@@@%%.             " << endl;
+    sleep(100);
+    cout << "              .%@@@+           #@@@%=             " << endl;
+    sleep(100);
+    cout << "            .+@@@@*            -@@@%#             " << endl;
+    sleep(100);
+    cout << "          :#@@@%==             .%@@@#             " << endl;
+    sleep(100);
+    cout << "           ::..                 .@@%*             " << endl;
+    sleep(100);
+    cout << "                                .@@%#             " << endl;
+    sleep(100);
+    cout << "                                *@@@%             " << endl;
+    sleep(100);
+
+    return 0;
+}
